e5d41a90bd6e79f560db5d98b215a88303a35773
DDRobinson-icstars <Drobinson@icstars.org>
2024-08-26 18:12:39 -0500

    Add files via upload

---
+++ Models/Comment.cs
@@ -0,0 +1,8 @@
+public class Comment {
+    public int Id {get; set;}
+    public int UserId { get;set;}
+    public bool CommentLevel {get; set;}
+    public long DraftId {get;set;}
+
+
+}
---
+++ Models/Draft.cs
@@ -0,0 +1,14 @@
+public class Draft {
+    public int Id {get; set;}
+
+    public string SubmitterId{get;set;}
+
+    public bool DraftType {get; set;}
+
+    public long DraftLog {get; set;}
+
+    
+
+
+
+}
