831d6dfefe28fa08e5d6fb3c8aa7c05bb2bc33d9
DDRobinson-icstars <Drobinson@icstars.org>
2024-09-01 18:10:40 -0500

    Convention update
    added navi's to the convention model

--- backend/EvaluatePro/Models/Convention.cs
+++ backend/EvaluatePro/Models/Convention.cs
@@ -2,5 +2,14 @@ public class Convention{
     public int id {get; set;}
     public string ConventionId {get; set;}
     public int CategoryId{get;set;}
+    public int FeedbackId { get; set; }
+    public int ScoreId { get; set; }
+    public int CommentID { get; set; }
+
+    public required Category Category { get; set; }
+    public required Feedback Feedback { get; set; }
+    public required Score  Score { get; set; }
+    public Comment? Comment { get; set; }
+
 
 }
