5394d9c6420075991cc8f39c8d0ee4b2a1d121ab
DDRobinson-icstars <Drobinson@icstars.org>
2024-09-01 17:53:20 -0500

    comment update
    added navi's to the comment model

--- backend/EvaluatePro/Models/Comment.cs
+++ backend/EvaluatePro/Models/Comment.cs
@@ -1,5 +1,11 @@
 public class Comment {
     public int Id {get; set;}
     public int UserId { get;set;}
-    public bool CommentLevel {get; set;}
+    public bool CommentType {get; set;}
+
+    public Submission? Submission { get; set; }
+    public Category? Category { get; set; }
+    public Convention? Convention { get; set; }
+
+
 }
