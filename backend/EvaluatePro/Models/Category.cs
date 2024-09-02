d618fb516e2fefdb2624fbbc437bc9a6631fd620
DDRobinson-icstars <Drobinson@icstars.org>
2024-09-02 13:30:31 -0500

    Final Migration set
    models are complete and fulled.

--- backend/EvaluatePro/Models/Category.cs
+++ backend/EvaluatePro/Models/Category.cs
@@ -3,13 +3,11 @@ public class Category
     public required string ConventionId{get;set;}
     public int SubmissionId {get;set;}
     public int ScoreId {get;set;}
-    public int CategoryComment { get; set; }
+    public int CommentId { get; set; }
 
     public required User User { get; set; }
     public required Score Score {get;set;}
     public Comment? Comment {get;set;}
     public required Submission Submission { get; set; }
     public List<Convention> Conventions {get;set;} = new List<Convention>();
-    
-
-}
+    }
